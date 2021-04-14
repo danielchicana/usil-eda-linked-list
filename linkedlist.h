7ca7823bb2f8a3f6b240c7cf15ff7afe027c866a
Angel Mejia <66014666+ins4w@users.noreply.github.com>
2021-04-14 07:13:10 -0500

    Linked List Header

---
+++ linkedlist.h
@@ -0,0 +1,32 @@
+#ifndef __QUEUE_H__
+#define __QUEUE_H__
+
+// #include
+// #include <vector>
+// using namespace std;
+
+template <typename T>
+class LinkedList
+{
+  private:
+  // members here
+  public:
+    void add(T &elem);
+    T remove();
+};
+
+// Comments #2
+template <typename T>
+void LinkedList<T>::add(T &elem)
+{
+
+}
+
+// Comments #3
+template <typename T>
+T LinkedList<T>::remove()
+{
+
+}
+
+#endif
